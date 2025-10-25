61e3b4a24e4753e3ce2bb919666d40d2ae8c16fa
ChristianP5 <christiankps111@gmail.com>
2025-10-25 15:49:20 +0700

    implemented Chat functionality v2

--- public/chat/chatById.js
+++ public/chat/chatById.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     
      /**
-      * 1) Check if the Chat ID Exists
+      * 1) Get Information of Chat and User
       * 2) Generate Inferences from that Chat
       * 3) Load the Chat Function
       */
@@ -21,9 +21,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     disableButton(submitButton)
 
      // 1)
+     chatId = window.location.pathname.split('/')[2]
+     userId = "TEST_USER"
 
      // 2)
-     chatId = window.location.pathname.split('/')[2]
+     
 
      const generateInferencesByChatId = async (chatId) => {
         const targetEndpoint = `/v1/chats/${chatId}/inferences`
@@ -76,7 +78,83 @@ document.addEventListener("DOMContentLoaded", async () => {
      enableButton(submitButton)
 
      // 3)
-     const loadChatFunction = async () => [
+    const loadChatFunction = async () => {
+      submitButton.addEventListener('click', async (e) => {
+        e.preventDefault()
 
-     ]
+        // Disable Submit Button
+        disableButton(submitButton)
+
+        // Extract and Empty the Question box
+        const questionInputElement = document.querySelector('#input-question') 
+        const question = questionInputElement.value
+        questionInputElement.value = ''
+
+
+        // Create a new Chat Item
+        const now = new Date();
+        const dateCreated = now.toUTCString();
+
+        const chatSpaceList = document.querySelector('#chat-space')
+        const itemElement = document.createElement('section')
+            itemElement.classList.add('row');
+            itemElement.innerHTML =`
+            <section class="container chat-item">
+              <section class="row question-sect">
+                <section class="container user-sect">
+                  <section class="row userinfo-sect">
+                    <p><b>${userId}</b> - <span>${dateCreated}</span></p>
+                  </section>
+                  <section class="row question-sect"><p>${question}</p></section>
+                </section>
+              </section>
+              <section class="row response-sect">
+                <section class="container ai-sect">
+                  <section class="row aiinfo-sect">
+                    <p><b>LLM (Ollama)</b> - <span>${dateCreated}</span></p>
+                  </section>
+                  <section class="row response-sect">
+                    <p class="genereated-result-text">Generating Response...</p>
+                  </section>
+                </section>
+              </section>
+            </section>
+          `
+
+          chatSpaceList.appendChild(itemElement)
+
+        const formData = JSON.stringify({
+            question: question
+        })
+
+        const targetEndpoint = `/v1/inference`
+        const result = await fetch(targetEndpoint, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: formData
+        })
+
+        const data = await result.json()
+
+        if(!result.ok){
+          alert(data.message)
+          enableButton(submitButton)
+          throw new Error(data.message)
+        }
+
+        // Extract the response
+        const response = data.data.response
+
+        // Display the response
+        const generatedResultTextElement = itemElement.querySelector(".genereated-result-text")
+        generatedResultTextElement.textContent = response
+        
+        // Re-enable the Button
+        enableButton(submitButton)
+
+      })
+    }
+    loadChatFunction()
 })
